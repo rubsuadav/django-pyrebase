8361f72aaa999cea79a68d78a2ffb040159dc410
rubsuadav <rsuarezdavid@gmail.com>
2023-10-23 12:35:10 +0200

    created structure of the proyect and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//local imports
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Profile from "./pages/Profile";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
-  return <div className="text-red-600">Hello World</div>;
+  return (
+    <div className="w-full h-full bg-gray-100">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
---
+++ frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <form className="w-full md:w-1/3 rounded-lg">
+        <div className="flex font-bold justify-center mt-6">
+          <img className="h-20 w-20 mb-3" src="https://dummyimage.com/64x64" />
+        </div>
+        <h2 className="text-2xl text-center text-gray-500 mb-8">Login</h2>
+        <div className="px-12 pb-10">
+          <div className="w-full mb-2">
+            <div className="flex items-center">
+              <input
+                type="text"
+                placeholder="Email Address"
+                className="
+                  w-full
+                  border
+                  rounded
+                  px-3
+                  py-2
+                  text-gray-700
+                  focus:outline-none
+                "
+              />
+            </div>
+          </div>
+          <div className="w-full mb-2">
+            <div className="flex items-center">
+              <input
+                type="password"
+                placeholder="Password"
+                className="
+                  w-full
+                  border
+                  rounded
+                  px-3
+                  py-2
+                  text-gray-700
+                  focus:outline-none
+                "
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="
+              w-full
+              py-2
+              mt-8
+              rounded-full
+              bg-blue-400
+              text-gray-100
+              focus:outline-none
+            "
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
